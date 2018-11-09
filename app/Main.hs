283cb475de4000a929d00a10cf06b65d44ad6ace
Tomas Monkevic <tomas.monkevic97@gmail.com>
2018-11-09 14:45:00 +0200

    Moved http functions to separate file

--- app/Main.hs
+++ app/Main.hs
@@ -2,26 +2,12 @@ module Main where
 
 {-# LANGUAGE OverloadedStrings #-}
 
-import Network.HTTP.Client
 import Data.Aeson
--- import Network.HTTP.Types.Status (statusCode)
-import qualified Data.ByteString.Char8 as S8
-import Data.Text (Text)
-import qualified Data.CaseInsensitive as CI
 import Moves
+import Http
 
 main :: IO ()
 main = do
-    manager <- newManager defaultManagerSettings
-
-    let testMoves = Moves ("D","3") (Just HIT)  (Just $ Moves ("A", "6") Nothing Nothing)
-
-    initialRequest <- parseRequest "http://battleship.haskell.lt/game/tm_test3/player/A"
-    let request = initialRequest { 
-        method = ("POST"), 
-        requestBody = RequestBodyLBS ( encode testMoves),
-        requestHeaders = [("Content-Type", "application/json+nomaps")]
-    }
-    response <- httpLbs request manager
-    -- putStrLn $ "The status code was: " ++ (show $ statusCode $ responseStatus response)
-    print $ responseBody response
+    let testMoves = Moves ("F","4") (Just HIT)  (Just $ Moves ("F", "1") Nothing Nothing)
+    response <- httpPost (encode testMoves)
+    print response
---
+++ src/Http.hs
@@ -0,0 +1,19 @@
+module Http where
+
+import Network.HTTP.Client
+import qualified Data.ByteString.Lazy.Char8 as LS8
+import Data.Text (Text)
+import qualified Data.CaseInsensitive as CI
+
+httpPost :: LS8.ByteString -> IO LS8.ByteString
+httpPost json = do
+    manager <- newManager defaultManagerSettings
+
+    initialRequest <- parseRequest "http://battleship.haskell.lt/game/tm_test3/player/A"
+    let request = initialRequest { 
+        method = ("POST"), 
+        requestBody = RequestBodyLBS json,
+        requestHeaders = [("Content-Type", "application/json+nomaps")]
+    }
+    response <- httpLbs request manager
+    return $ responseBody response
