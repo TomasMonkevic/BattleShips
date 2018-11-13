66ce154fe907c947187dafd9c95a0c472b41453b
Tomas Monkevic <tomas.monkevic97@gmail.com>
2018-11-13 16:29:56 +0200

    Added todo list

--- src/Http.hs
+++ src/Http.hs
@@ -9,7 +9,7 @@ gameUrl :: String
 gameUrl = "http://battleship.haskell.lt/game/"
 
 gameId :: String
-gameId = "tm_test13"
+gameId = "tm_test14"
 
 httpPost :: [Char] -> LS8.ByteString -> IO LS8.ByteString
 httpPost player json = do
