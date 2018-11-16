99175c5cf633dd7b7dee61f4ae5c63148c5d5b4b
Tomas Monkevic <tomas.monkevic97@gmail.com>
2018-11-16 23:49:06 +0200

    Added check if moves are availavle

--- src/BattleShips.hs
+++ src/BattleShips.hs
@@ -64,14 +64,23 @@ play :: Integer -> String -> Maybe Moves -> IO()
 play turn player m = do
     if turn == 0
     then do
-        moveCoord <- getNextMove $ availableMoves m (if player == "A" then 0 else 1)
-        let move = Moves moveCoord (isHit m) m
-        postResponse <- httpPost player (encode move)
-        print postResponse
-        print move
-        play 1 player (Just move)
+        let am = availableMoves m (if player == "A" then 0 else 1)
+        case am of 
+            [] -> do 
+                return ()
+            am2 -> do
+                moveCoord <- getNextMove am2
+                let move = Moves moveCoord (isHit m) m
+                postResponse <- httpPost player (encode move)
+                print postResponse
+                print move
+                play 1 player (Just move)
     else do
         getResponse <- (httpGet player)
-        print getResponse
-        let getMoves = decode getResponse :: Maybe Moves
-        play 0 player getMoves
+        if getResponse == "No move available at the moment"
+        then do
+            return ()
+        else do
+            print getResponse
+            let getMoves = decode getResponse :: Maybe Moves
+            play 0 player getMoves
